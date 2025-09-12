0af970e2d3d64c154f1ed8b5e9d01ccf5452991c
Vladislav Lantsev <vladislavii47@gmail.com>
2025-09-12 16:57:38 +0400

    v.2.0.1: main_script.py show-ready version

--- sources/main_script.py
+++ sources/main_script.py
@@ -10,19 +10,17 @@ from learn_model import (autoencoder_train,
                          )
 
 from get_post_embeddings import make_roberta_embeddings, get_128d_embeddings
-from get_features_table import (df_to_sql, 
-                                csv_to_sql, 
+from get_features_table import (csv_to_sql, 
                                 FEED_FEATURES_TYPE_MAP,
                                 USER_FEATURES_TYPE_MAP,
                                 POST_FEATURES_TYPE_MAP)
 from dotenv import load_dotenv
-import pandas as pd
 import os
 
-N_EPOCHS = 3
-LEARNING_RATE = 25e-5
+N_EPOCHS = 10
+LEARNING_RATE = 5e-4
 BATCH_SIZE=256
-IS_NEW_MODEL=False
+IS_NEW_MODEL=True
 
 if __name__ == "__main__":
 
@@ -32,27 +30,27 @@ if __name__ == "__main__":
     # Load env variables
     load_dotenv()
     
-#     # Load RoBerta and prepare text embeddings 768d
-#     df_post_embed = make_roberta_embeddings()
+    # Load RoBerta and prepare text embeddings 768d
+    df_post_embed = make_roberta_embeddings()
     
-#     # Train autoencoder
-#     autoencoder_model, _ = autoencoder_train(df_post_embed)
+    # Train autoencoder
+    autoencoder_model, _ = autoencoder_train(df_post_embed)
     
-#     # Create dataset object form post embeddings dataframe
-#     post_dataset = Post_Data(df_post_embed)
+    # Create dataset object form post embeddings dataframe
+    post_dataset = Post_Data(df_post_embed)
     
-#     # Receive 128d embeddings
-#     df_post_128d = get_128d_embeddings(autoencoder_model, post_dataset)
+    # Receive 128d embeddings
+    df_post_128d = get_128d_embeddings(autoencoder_model, post_dataset)
 
     df_post_128d = get_embedd_df(is_csv=True)
 
     # Fetch DB data for user, post and feed
     user_features, post_features, feed_encoded = get_vector_df(df_post_128d,
                                                                feed_n_lines=1512000,
-                                                               is_csv=True)
+                                                               is_csv=False)
 
-    # Create dicts with user's histories of interactions - random max_history=50 interactions per user
-    user_hist_dict = build_user_histories(feed_encoded)
+    # Create dicts with user's histories of interactions - random max_history=100 interactions per user
+    user_hist_dict = build_user_histories(feed_encoded, max_history=100)
 
     # Create dataset for learning with self-attention, with last lim_hist=8 interactions before the current one
     dataset = InteractionDatasetWithHistory(feed_encoded,
@@ -95,19 +93,19 @@ if __name__ == "__main__":
                                              lr=LEARNING_RATE,
                                              is_new_model=IS_NEW_MODEL)
 
-#     # Send features df's to the SQL database - for web service operation
-#     csv_to_sql('feed_df_encoded_for_2towers.csv',
-#           os.getenv('FEED_FEATURES_NN'),
-#           FEED_FEATURES_TYPE_MAP)
+    # Send features df's to the SQL database - for web service operation
+    csv_to_sql('feed_df_encoded_for_2towers.csv',
+          os.getenv('FEED_FEATURES_NN'),
+          FEED_FEATURES_TYPE_MAP)
 
-#     csv_to_sql('user_df_encoded_for_2towers.csv',
-#                 os.getenv('USER_FEATURES_NN'), 
-#                 USER_FEATURES_TYPE_MAP, 
-#                 )
+    csv_to_sql('user_df_encoded_for_2towers.csv',
+                os.getenv('USER_FEATURES_NN'), 
+                USER_FEATURES_TYPE_MAP 
+                )
 
-#     csv_to_sql('post_df_encoded_for_2towers.csv',
-#                 os.getenv('POST_FEATURES_NN'), 
-#                 POST_FEATURES_TYPE_MAP, 
-#                 )
+    csv_to_sql('post_df_encoded_for_2towers.csv',
+                os.getenv('POST_FEATURES_NN'), 
+                POST_FEATURES_TYPE_MAP
+                )
 
 
